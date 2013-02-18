0b20560e53e1644c68e937bd7e09e7857af7c0f9
bhavani <bhavani.gdm@gmail.com>
2013-02-17 22:32:42 -0800

    filtered data

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # The following is for twitter
-  provider :twitter, "HcerxT89oaW35Gtq7g7gw", "eslfwCUDKIV7uQ7zSfYoRbwTcRV4qCyliXbNPuw"
+  provider :twitter, "XXXXXX", "YYYYYYYYYYYY"
 
   # If you want to also configure for additional login services, they would be configured here.
 end
